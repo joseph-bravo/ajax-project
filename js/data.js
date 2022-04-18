909da28fab5beccd10243179138048facbc331fe
Joseph Bravo <josephangelobravo@gmail.com>
2022-04-18 20:44:14 +0000

    created html css structure added ID to card title and card image defined function that pulls all monster cards from API and places into object

--- js/data.js
+++ js/data.js
@@ -1,1 +1,18 @@
 /* exported data */
+var cardData = {};
+
+function pullAllCardData() {
+  var cardXHR = new XMLHttpRequest();
+  cardXHR.open('GET', 'https://db.ygoprodeck.com/api/v7/cardinfo.php' +
+  '?' + 'format=TCG' + '&' +
+  'attribute=dark,earth,fire,light,water,wind,divine'
+  );
+  cardXHR.responseType = 'json';
+  cardXHR.send();
+  cardXHR.addEventListener('load', function () {
+    cardData = cardXHR.response;
+    console.log(cardData);
+  });
+}
+
+pullAllCardData();
